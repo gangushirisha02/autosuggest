f23bae8129ef7837fbd9031ef997f59dabe32283
Gangu Shirisha <gangushirisha2007@gmail.com>
2026-07-03 23:08:53 +0530

    home-image card

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,26 @@
 console.log("hello from js");
+const users = [
+  {
+    "name" :"Shirisha",
+    "role" : "Student at BVRITH",
+    "image" : "./images/siri.jpg"
+  },
+  {
+    "name" :"siri",
+    "role" : "siri the swag",
+    "image" : "./images/siri1.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("card-img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  // role
+  document.getElementById("card-role").innerText=users[curUserId].role;
+}
